d1f4d09455a5fb9c07c989b1b0b97991bd5e834b
tigua <corwin.dong@hotmail.com>
2021-08-12 15:25:18 +0800

    chapter05 half

--- j/chapter05.ijs
+++ j/chapter05.ijs
@@ -9,4 +9,14 @@ A =: randnr_mt_ 2 5
 At =: |: A
 
 
+NB. Code block 5.3
+I =: e. i. 4
+O =: 4 4 $ 1
+Z =: 4 4 $ 0
+
+NB. Code block 5.5
+D =: diagmat_mt_ 1 2 3 4 5
+R =: randnr_mt_ 3 4
+d =: diag R
+
 NB. Code challenges
--- julia/chapter05.jl
+++ julia/chapter05.jl
@@ -1,9 +1,23 @@
 # CHAPTER: Matrices (chapter 5)
 # Code
 
+# Load package
+using LinearAlgebra
+
 # Code block 5.1
 A = randn(2, 5)
 At1 = A'
 At2 = transpose(A)
+
+# Code block 5.3
+IM = Matrix(1.0I, 4, 4)
+O = ones(4, 4)
+Z = zeros(4, 4)
+
+# Code block 5.5
+D = diagm([1, 2, 3, 4, 5])  # diagonal matrix
+R = randn(3, 4)
+d = diag(R)  # diagonal elements
+
 
 # Code challenges
