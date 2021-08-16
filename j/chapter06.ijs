9e2d8bf498cf8830bb6c3e086d9aca7f97670b26
tigua <corwin.dong@hotmail.com>
2021-08-16 17:07:58 +0800

    finish chapter06

--- j/chapter06.ijs
+++ j/chapter06.ijs
@@ -16,5 +16,46 @@ C1 =: A mp_mt_ B
 C2 =: B mp_mt_ A
 C1 -: C2
 
+NB. Code block 6.5
+M1 =: randnr_mt_ 4 3
+M2 =: randnr_mt_ 4 3
+M1 * M2
+
+NB. Code block 6.7
+A =: 2 3 $ >: i. 6
+, |: A
+
+NB. Code block 6.9
+A =: randnr_mt_ 4 3
+B =: randnr_mt_ 4 3
+]f =: trace_mt_ (|: A) mp_mt_ B
+
+NB. Code block 6.11
+A =: rannr_mt_ 4 3
+norms_mt_ A
+
 
 NB. Code challenges
+NB. Challenge 1
+
+
+NB. Challenge 2
+D =: diagmat_mt_ >: i.4
+A =: randnr_mt_ 4 4
+C1 =: D * A
+C2 =: D mp_mt_ A
+(,. diag C1) ; ,. diag C2
+
+NB. Challenge 3
+A =: diagmat_mt_ randu_mt_ 3
+C1 =: ((|: A) + A) % 2
+C2 =: (|: A) mp_mt_ A
+C1 - %: C2
+
+NB. Challenge 4
+m =: 5
+A =: randnr_mt_ m, m
+v =: randnr_mt_ m, 1
+LHS =: norms_mt_ A mp_mt_ v
+RHS =: (norms_mt_ A) * (norms_mt_ v)
+RHS - LHS
--- julia/chapter06.jl
+++ julia/chapter06.jl
@@ -1,5 +1,6 @@
 # CHAPTER: Matrix multiplication (chapter 6)
 # Code
+using LinearAlgebra
 
 # Code block 6.1
 M1 = randn(4, 3)
@@ -13,5 +14,53 @@ C1 = A * B
 C2 = B * A
 C1 == C2
 
+# Code block 6.5
+M1 = randn(4, 3)
+M2 = randn(4, 3)
+M1 .* M2
+
+# Code block 6.7
+A = [1 2 3; 4 5 6]
+A[:]
+
+# Code block 6.9
+A = randn(4, 3)
+B = randn(4, 3)
+f = tr(A' * B)
+
+# Code block 6.11
+A = randn(4, 3)
+norm(A)
+
 
 # Code challenges
+# Challenge 1
+A = randn(2, 4)
+B = randn(4, 3)
+C1 = zeros(2, 3)
+for i in 1:4
+    C1 += A[:, [i]] * B[[i], :]
+end
+C1 - A * B  # show equality
+
+
+# Challenge 2
+D = diagm(1:4)
+A = randn(4, 4)
+C1 = D .* A
+C2 = D * A
+[diag(C1) diag(C2)]
+
+# Challenge 3
+A = diagm(rand(3))
+C1 = (A' + A) / 2
+C2 = A' * A
+C1 - sqrt(C2)
+
+# Challenge 4
+m = 5
+A = randn(m, m)
+v = randn(m)
+LHS = norm(A * v)
+RHS = norm(A) * norm(v)
+RHS - LHS  # should always be positive
