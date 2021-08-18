27a54f6da0e87d987e81df01de275388b42be221
tigua <corwin.dong@hotmail.com>
2021-08-18 15:55:56 +0800

    init chapter06

--- j/chapter07.ijs
+++ j/chapter07.ijs
@@ -1,5 +1,8 @@
 NB. CHAPTER: Matrix rank (chapter 7)
 NB. Code
 
+NB. load package
+load 'math/mt'
+
 
 NB. Code challenges
--- julia/chapter07.jl
+++ julia/chapter07.jl
@@ -1,5 +1,12 @@
 # CHAPTER: Matrix rank (chapter 7)
 # Code
 
+# load package
+using LinearAlgebra
+
+
+# Code block 7.1
+A = randn(3, 6)
+r = rank(A)
 
 # Code challenges
