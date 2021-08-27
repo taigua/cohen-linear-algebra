bcb8724b9916bb01fa1895249b0e8ad5592f008e
tigua <corwin.dong@hotmail.com>
2021-08-27 12:56:10 +0800

    init chapter08

---
+++ j/chapter08.ijs
@@ -0,0 +1,5 @@
+NB. CHAPTER: Matrix spaces (chapter 8)
+NB. Code
+
+NB. load package
+load 'math/mt'
---
+++ julia/chapter08.jl
@@ -0,0 +1,9 @@
+# CHAPTER: Matrix spaces (chapter 8)
+# Code
+
+# load package
+using LinearAlgebra
+
+
+
+# Code Challenges
